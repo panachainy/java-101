d0dbd035a9599d793813b51b55a4e8a6de0f9019
Panachai <panachai.ny@gmail.com>
2021-06-07 00:09:51 +0700

    fix test

--- src/test/java/panachainy/java/app/controller/VersionControllerTests.java
+++ src/test/java/panachainy/java/app/controller/VersionControllerTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,7 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
+@WebMvcTest(value = VersionController.class)
 @AutoConfigureMockMvc
 public class VersionControllerTests {
     @Autowired
