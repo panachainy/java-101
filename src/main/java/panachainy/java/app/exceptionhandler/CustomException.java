82264c5e8a303c08b0d358f8c05ac4c2ca1a8409
chanchit <chlear@celestica.com>
2021-06-14 18:05:42 +0700

    Created ExceptionHandler
    Created Exception Files and testExceptionController

---
+++ src/main/java/panachainy/java/app/exceptionhandler/CustomException.java
@@ -0,0 +1,7 @@
+package panachainy.java.app.exceptionhandler;
+
+public class CustomException extends RuntimeException {
+    public CustomException(String message) {
+        super(message);
+    }
+}
